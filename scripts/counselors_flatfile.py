9d4074961812c4cd86786056f5f8bf63b74cfcc1
Chipp Jansen <chipp.jansen@kcl.ac.uk>
2018-09-15 17:46:31 +0100

    added service and publisher handlers

--- scripts/counselors_flatfile.py
+++ scripts/counselors_flatfile.py
@@ -5,20 +5,39 @@
 
 import rospy
 
-from counselors.msg import Counselor
+from counselors.msg import Counselor, Available
+from counselors.srv import Next
 
-#def picked_up():
-#    global pub
-#    pub_str = "PICKED UP %s" % rospy.get_time()
-#    rospy.loginfo(pub_str)
-#    pub.publish(Handset(stamp=rospy.Time.now(), picked_up=True))
+def next_counselor(req):
+    rospy.loginfo("Next Counselor")
+    # stub counselor
+    c = Counselor(name="Person 1", loc="here", loc="+123456789")
+    return Next(c)
 
+def counselor_timeline(event):
+    global pub
+    rospy.loginfo("Updating Counselors %s" % rospy.get_time())
+
+    # how many counselors are currently available
+    pub.publish(Available(num_available=1))
 
 def counselors_manager():
-    # Set-up Publisher
     global pub
-    # pub = rospy.Publisher('hook', Handset, queue_size=10)
+
     rospy.init_node('counselors_manager', anonymous=True)
+
+    # availability publishing
+    pub = rospy.Publisher('counselors/available', Available, queue_size=10)
+
+    # next counselor service
+    s = rospy.Service('next', Next, next_counselor)
+
+    # monitor the counselor timeline every 30 seconds
+    rospy.Timer(rospy.Duration(30), counselor_timeline)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
 
 if __name__ == '__main__':
     try:
